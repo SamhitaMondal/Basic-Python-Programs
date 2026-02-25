084329485391f2a07cb63f7bda1002ef171c1972
SamhitaMondal <samhita.mondal2005@gmail.com>
2026-02-25 13:00:04 +0530

    Update calculator_gui.py

--- calculator_gui.py
+++ calculator_gui.py
@@ -1,16 +1,14 @@
 # GRAPHICAL CALCULATOR
 import tkinter as tk
-#GLOBAL DECLARATION
 
 
-root =tk.Tk()   #CREATE MAIN WINDOW
-root.title("CALCULATOR")  #SET TITLE
-root.geometry("300x400")   #SET WINDOW HEIGHT AND WIDTH
+root =tk.Tk()   
+root.title("CALCULATOR")  
+root.geometry("300x400")   
 root.configure(background="blue")
 
 expression=" "
-equation= tk.StringVar()   #TKINTER VARIABLE(CONNECTED TO ENTRY)
-
+equation= tk.StringVar()   
 def press(num):
     global expression
     expression+=str(num) #ADD THE BUTTON'S VALUE(NUM) TO THE EXPRESSION STRING
@@ -22,18 +20,18 @@ def clear():
     equation.set(" ") #update the screen to empty
 
 
-#entry box(Calculator screen)
+
 entry = tk.Entry(root,textvariable=equation,font=("Arial",20),bd=10,relief="ridge")
 entry.grid(row=0,column=0,columnspan=4,pady=10)
 
-#Digits Layout
+
 digits=[('7',1,0),('8',1,1),('9',1,2),
         ('4',2,0),('5',2,1),('6',2,2),
         ('1',3,0),('2',3,1),('3',3,2),
         ('0',4,1)]
 for (text,row,col) in digits:
-    button=tk.Button(root,text=text,width=5,height=2,font=("Arial",16),command=lambda t=text:press(t))#false function created which waits till the button is pressed to call that function
-    button.grid(row=row,column=col,padx=5,pady=5)#pady=adds vertical padding and padx=adds horizontal padding
+    button=tk.Button(root,text=text,width=5,height=2,font=("Arial",16),command=lambda t=text:press(t))
+    button.grid(row=row,column=col,padx=5,pady=5)
 
 #Clear button
 
@@ -56,9 +54,9 @@ div_btn.grid(row=4,column=3,pady=5)
 def equalpress():
     global expression
     try:
-        result = str(eval(expression))   # evaluate expression
-        equation.set(result)             # show result
-        expression = result              # allow continuing calculation
+        result = str(eval(expression))   
+        equation.set(result)             
+        expression = result            
     except:
         equation.set("Error")
         expression = ""
@@ -66,4 +64,5 @@ def equalpress():
 
 equal_btn=tk.Button(root,text="=",width=10,height=2,command=equalpress)
 equal_btn.grid(row=4,column=2,pady=10)
+
 root.mainloop()
